cd2c1b22cdc946ae8a2e018878d091a51480b34c
Elena Lemeskina <gogulinaelena@yandex.ru>
2022-07-23 23:14:26 +0300

    Files Main, Readme.md

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SalesManager period = new SalesManager(new int[]{68, 55, 98, 35, 87, 100});
+
+        System.out.println("Максимальное количество продаж за период: " + period.max() + " шт.");
+
+    }
+
+}
